1de808aa8c4d1f78e7328b78b04c974b0a19be8e
Nick Karpov <nick@nickkarpov.com>
2022-11-22 14:29:33 -0800

    Routes (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "no-else-return": 0,
+    "react/jsx-props-no-spreading": 0,
     "react/prop-types": 0,
     "import/prefer-default-export": 0,
     "react/function-component-definition": [
