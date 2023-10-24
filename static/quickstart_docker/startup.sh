c79bbc1fd08520dbfab1496934ca66edfb302a95
Scott Haines <newfrontcreative@gmail.com>
2023-10-24 14:41:51 -0700

    updated the base docker to delta 3.0 and spark 3.5.  (#60)

--- static/quickstart_docker/startup.sh
+++ static/quickstart_docker/startup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+source "$HOME/.cargo/env"
+
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='lab --ip=0.0.0.0'
+export DELTA_SPARK_VERSION='3.0.0'
+export DELTA_PACKAGE_VERSION=delta-spark_2.12:${DELTA_SPARK_VERSION}
 
 $SPARK_HOME/bin/pyspark --packages io.delta:${DELTA_PACKAGE_VERSION} \
---conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
---conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+  --conf "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp -Divy.home=/tmp -Dio.netty.tryReflectionSetAccessible=true" \
+  --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
